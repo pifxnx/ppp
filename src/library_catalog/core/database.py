84ff21d6f03fab03ba44093c9cfc19b8528b6492
Егушев <kirikott11@gmail.com>
2026-07-03 13:07:47 +0700

    add base repository methods

--- src/library_catalog/core/database.py
+++ src/library_catalog/core/database.py
@@ -8,7 +8,7 @@ class Base(DeclarativeBase):
 
 
 engine = create_async_engine(
-    settings.database_url_str,
+    settings.database_url,
     pool_size = settings.database_pool_size,
     echo=settings.debug,
 )
--- src/library_catalog/data/repositories/base_repository.py
+++ src/library_catalog/data/repositories/base_repository.py
@@ -1,6 +1,7 @@
 from typing import Generic, TypeVar, Type
 from uuid import UUID
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select, delete, update
 
 
 T = TypeVar('T')
@@ -11,27 +12,55 @@ class BaseRepository(Generic[T]):
         self.model = model
 
     async def create(self, **kwargs) -> T:
-        '''Создать запись'''
-        pass
+        obj = self.model(**kwargs)
+        self.session.add(obj)
+        await self.session.commit()
+        await self.session.refresh()
+        return obj
+
 
     async def get_by_id(self, id: UUID) -> T | None:
-        '''Получить по id'''
-        pass
+        obj = await self.session.get(self.model, id)
+        return obj
 
     async def update(self, id: UUID, **kwargs) -> T | None:
-        '''Обновить запись'''
-        pass
+        obj = await self.session.get(self.model, id)
+        if not obj:
+            return None
+        
+        for k, v in kwargs.items():
+            setattr(obj, k, v)
+        
+        # query = (
+        #     update(self.model).
+        #     where(self.model.id == id).
+        #     values(**kwargs)
+        # )
+        # await self.session.execute(query)
+        await self.session.commit()
+        return obj
+        
 
     async def delete(self, id: UUID) -> bool:
-        '''Удалить запись'''
-        pass
+        obj = await self.session.get(self.model, id)
+        if not obj:
+            return False
+        
+        # stmt = delete(self.model).where(self.model.id == id)
+        # await self.session.execute(stmt)
+        await self.session.delete(obj)
+        await self.session.commit()
+        return True
+        
 
     async def get_all(
             self,
             limit: int = 100,
             offset: int = 0
     ) -> list[T]:
-        '''Получить все записи с пагинацией'''
-        pass
+        query = select(self.model).limit(limit).offset(offset)
+        res = await self.session.execute(query)
+        res = res.scalars().all()
+        return res
     
 
